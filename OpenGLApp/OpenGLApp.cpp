0a34c0b786ad9ad0a643ce17318b0a29e9dd2172
HaroldWSchranz <Harold.Schranz@gmail.com>
2023-03-12 01:39:34 +1100

    Updates
    Trying to build

--- OpenGLApp/OpenGLApp.cpp
+++ OpenGLApp/OpenGLApp.cpp
@@ -1,20 +1,51 @@
 // OpenGLApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "glad.h"  
+#include <GLFW/glfw3.h> 
+
 #include <iostream>
+
+const int windowWidth = 1280;
+const int windowHeight = 720;
+int main() {
+
+    if (glfwInit() == false)
+        return -1;
+
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight,
+        "Computer Graphics",
+        nullptr, nullptr);
+
+    if (window == nullptr) {
+        glfwTerminate();
+        return -2;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // the rest of our code goes here! 
+
+
+    if (!gladLoadGL()) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -3;
+    }
+
+    printf("GL: %i.%i\n", GLVersion.major, GLVersion.minor);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
+/*
 
 int main()
 {
     std::cout << "Hello World!\n";
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+*/
