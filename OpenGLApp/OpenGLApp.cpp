daba7665c20dc02d593187f65caadec30d170811
HaroldWSchranz <Harold.Schranz@gmail.com>
2023-03-12 15:49:33 +1100

    a grid displayed in 3-D.
    computer-graphics-building-an-opengl-application: a grid displayed in 3-D.

--- OpenGLApp/OpenGLApp.cpp
+++ OpenGLApp/OpenGLApp.cpp
@@ -46,18 +46,51 @@ int main() {
     glClearColor(0.25f, 0.25f, 0.25f, 1);
     glEnable(GL_DEPTH_TEST); // enables the depth buffer
 
+    Gizmos::create(10000, 10000, 0, 0);
+    mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+    mat4 projection = glm::perspective(glm::pi<float>() * 0.25f,
+        16 / 9.f, 0.1f, 1000.f);
+
     while (glfwWindowShouldClose(window) == false &&
         glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // our game logic and update code goes here! 
+        while (glfwWindowShouldClose(window) == false &&
+            glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            Gizmos::clear();
+
+            Gizmos::addTransform(glm::mat4(1));
+
+            vec4 white(1);
+            vec4 black(0, 0, 0, 1);
+
+            for (int i = 0; i < 21; ++i) {
+                Gizmos::addLine(vec3(-10 + i, 0, 10),
+                    vec3(-10 + i, 0, -10),
+                    i == 10 ? white : black);
+
+                Gizmos::addLine(vec3(10, 0, -10 + i),
+                    vec3(-10, 0, -10 + i),
+                    i == 10 ? white : black);
+            }
+
+            Gizmos::draw(projection * view);
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
         // so does our render code! 
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    Gizmos::destroy();
 
     glfwDestroyWindow(window);
     glfwTerminate();
